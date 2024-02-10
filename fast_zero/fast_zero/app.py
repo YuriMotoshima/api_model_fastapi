4483cf929e27e83beef47b7e47706e448747eeef
YuriMotoshima <yurimotoshima@gmail.com>
2024-02-10 19:36:10 -0300

    update: lint

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,19 +1,19 @@
 from fastapi import FastAPI
-
-from fast_zero.schemas import (Message, UserSchema, UserPublic, UserDB)
+from fast_zero.schemas import Message, UserSchema, UserPublic, UserDB
 
 app = FastAPI()
 
+
 database = []
+
 
 @app.get('/', status_code=200, response_model=Message)
 def read_root():
-    return {'message': 'Olá mundo'}
+    return {'message': 'Olá Mundo!'}
+
 
 @app.post('/users/', status_code=201, response_model=UserPublic)
 def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1) 
-
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
     database.append(user_with_id)
-
     return user_with_id
