6e2293efee6c319530dd770017cd782f5dbfddca
Yuri Motoshima <yuri.motoshima@accenture.comm>
2024-03-25 16:21:00 -0300

    update

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fast_zero.schemas import Message, UserSchema, UserList, UserPublic, UserDB
 
 app = FastAPI()
@@ -22,3 +22,24 @@ def create_user(user: UserSchema):
     user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
     database.append(user_with_id)
     return user_with_id
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
